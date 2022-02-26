cf5d64d0bee3c6d13543770f2cfc6721b4a1542e
owoDra <85442566+owoDra@users.noreply.github.com>
2022-02-26 14:24:37 +0900

    Initial commit

---
+++ Src/index.js
@@ -0,0 +1,459 @@
+// ======================
+//    メニューリスト
+// ======================
+
+// ===== メニューコンテンツ =====
+
+// 前菜
+var Appetizer = [{
+    // 0
+    title:"枝豆の塩ゆで",
+    price:300
+},{
+    // 1
+    title:"むき枝豆と高菜炒め",
+    price:300
+},{
+    // 2
+    title:"冷やしトマト",
+    price:300
+},{
+    // 3
+    title:"カリカリ上げ豆腐の中華風ソース",
+    price:300
+},{
+    // 4
+    title:"四川ザーサイ",
+    price:300
+},{
+    // 5
+    title:"季節野菜の甘酢漬け",
+    price:400
+},{
+    // 6
+    title:"ピータン豆腐の甘酢ソース",
+    price:400
+},{
+    // 7
+    title:"イカげその竜田揚げ",
+    price:400
+},{
+    // 8
+    title:"ゆで豚肉の薄切り香味ソース",
+    price:500
+},{
+    // 9
+    title:"豚バラ肉の紹興酒粕焼き",
+    price:600
+},{
+    // 10
+    title:"味の家特製海鮮サラダ",
+    price:600
+},{
+    // 11
+    title:"蒸し鶏細切り特製胡麻ソースかけ",
+    price:600
+},{
+    // 12
+    title:"クラゲの冷菜",
+    price:600
+},{
+    // 13
+    title:"味の家特製冷菜盛り合わせ",
+    price:900
+}];
+
+// 麺類
+var Noodle = [{
+        // 0
+        title:"五目あんかけかた焼きそば",
+        price:780
+    },{
+        // 1
+        title:"五目入り焼きビーフン",
+        price:780
+    },{
+        // 2
+        title:"味の家海鮮焼きそば",
+        price:880
+}];
+
+// ご飯類
+var Rice = [{
+    // 0
+    title:"海鮮入りお粥",
+    price:580
+},{
+    // 1
+    title:"五目あんかけご飯",
+    price:580
+},{
+    // 2
+    title:"豚肉とピーマン細切りあんかけご飯",
+    price:680
+},{
+    // 3
+    title:"ブラビ飯",
+    price:680
+},{
+    // 4
+    title:"五目チャーハン",
+    price:680
+},{
+    // 5
+    title:"海老チャーハン",
+    price:780
+},{
+    // 6
+    title:"福建チャーハン",
+    price:780
+},{
+    // 7
+    title:"黄金チャーハン",
+    price:800
+},{
+    // 8
+    title:"広東風牛あんかけチャーハン",
+    price:880
+},{
+    // 9
+    title:"広東風ロースカツ土鍋ご飯",
+    price:880
+},{
+    // 10
+    title:"豚角煮土鍋ご飯",
+    price:880
+}];
+
+// 海の幸料理
+var Sea = [{
+    // 0
+    title:"墨イカの四川風辛子炒め",
+    price:800
+},{
+    // 1
+    title:"白身魚の竜田揚げ中華風ソース",
+    price:800
+},{
+    // 2
+    title:"白身魚の切り身揚げ甘酢ソース",
+    price:800
+},{
+    // 3
+    title:"かに玉",
+    price:800
+},{
+    // 4
+    title:"芝海老のチリソース炒め",
+    price:900
+},{
+    // 5
+    title:"芝海老のマヨネーズ和え",
+    price:900
+},{
+    // 6
+    title:"海老とカシューナッツ炒め",
+    price:900
+},{
+    // 7
+    title:"海鮮あんかけおこげ",
+    price:900
+},{
+    // 8
+    title:"ホタテとブロッコリー炒め",
+    price:1280
+},{
+    // 9
+    title:"海鮮三種炒め",
+    price:1280
+},{
+    // 10
+    title:"大正海老のチリソース煮",
+    price:1280
+},{
+    // 11
+    title:"ホタテの辛子炒め",
+    price:1280
+},{
+    // 12
+    title:"アワビのオイスターソース煮",
+    price:3000
+},{
+    // 13
+    title:"フカヒレの姿煮",
+    price:4000
+}];
+
+// 鶏肉料理
+var Chicken = [{
+    // 0
+    title:"手羽のピリ辛揚げ",
+    price:400
+},{
+    // 1
+    title:"若鶏のからあげ",
+    price:400
+},{
+    // 2
+    title:"鶏カツのタルタルソース",
+    price:400
+},{
+    // 3
+    title:"鶏肉の目切りと銀杏炒め",
+    price:700
+},{
+    // 4
+    title:"鶏肉の目切りの辛子みそ炒め",
+    price:700
+},{
+    // 5
+    title:"チキン竜田揚げの甘酢醤油",
+    price:700
+}];
+
+// 豚肉料理
+var Pig = [{
+    // 0
+    title:"中華風豚ロースカツ揚げ",
+    price:600
+},{
+    // 1
+    title:"豚レバーとニラ炒め",
+    price:600
+},{
+    // 2
+    title:"豚肉ときくらげの玉子炒め",
+    price:700
+},{
+    // 3
+    title:"豚バラ肉とキャベツの辛味みそ炒め",
+    price:700
+},{
+    // 4
+    title:"細切り豚肉とピーマン炒め",
+    price:800
+},{
+    // 5
+    title:"細切り豚肉とザーサイ炒め",
+    price:800
+},{
+    // 6
+    title:"黒酢酢豚",
+    price:800
+},{
+    // 7
+    title:"豚バラ肉の角切り醤油煮込み",
+    price:800
+}];
+
+// 牛肉料理
+var Cow = [{
+    // 0
+    title:"牛肉とピーマンの細切り炒め",
+    price:900
+},{
+    // 1
+    title:"和牛ほほ肉のビール煮",
+    price:900
+},{
+    // 2
+    title:"牛肉とフクロ茸の辛子炒め",
+    price:900
+},{
+    // 3
+    title:"おこげ入り牛肉野菜炒め",
+    price:900
+},{
+    // 4
+    title:"牛肉の黒胡椒炒め",
+    price:900
+},{
+    // 5
+    title:"牛角煮飯",
+    price:900
+}];
+
+// 豆腐料理
+var Tofu = [{
+    // 0
+    title:"揚げ豆腐の蟹肉あんかけ",
+    price:700
+},{
+    // 1
+    title:"揚げ豆腐の五目煮込み",
+    price:700
+},{
+    // 2
+    title:"海鮮入り豆腐煮込み",
+    price:700
+},{
+    // 3
+    title:"五目入り豆腐煮込み",
+    price:600
+},{
+    // 4
+    title:"麻婆豆腐",
+    price:600
+},{
+    // 5
+    title:"黒麻婆豆腐",
+    price:700
+}];
+
+// 野菜料理
+var Veg = [{
+    // 0
+    title:"野菜の塩味炒め",
+    price:600
+},{
+    // 1
+    title:"ニラ焼き玉子",
+    price:600
+},{
+    // 2
+    title:"チンゲン菜のスープ煮",
+    price:700
+},{
+    // 3
+    title:"レタスのホイル",
+    price:700
+},{
+    // 4
+    title:"ナスと豚ひき肉の辛子土鍋",
+    price:700
+},{
+    // 5
+    title:"ブロッコリーと蟹肉あんかけ",
+    price:700
+},{
+    // 6
+    title:"レタスとアワビ茸のうま煮",
+    price:800
+}];
+
+// 点心
+var Tensin = [{
+    // 0
+    title:"自家製焼き餃子 (5ヶ)",
+    price:480
+},{
+    // 1
+    title:"自家製スープ入り水餃子 (6ヶ)",
+    price:480
+},{
+    // 2
+    title:"海老入りシュウマイ (4ヶ)",
+    price:480
+},{
+    // 3
+    title:"揚げ香味春巻き (4ヶ)",
+    price:480
+},{
+    // 4
+    title:"海老餃子 (4ヶ)",
+    price:480
+},{
+    // 5
+    title:"サクサク揚げワンタン (4ヶ)",
+    price:480
+},{
+    // 6
+    title:"イカ団子 (4ヶ)",
+    price:600
+},{
+    // 7
+    title:"特製スープ入り小籠包 (3ヶ)",
+    price:600
+},{
+    // 8
+    title:"フカヒレ餃子 (4ヶ)",
+    price:600
+}];
+
+// デザート
+var Desert = [{
+    // 0
+    title:"杏仁豆腐",
+    price:300
+},{
+    // 1
+    title:"マンゴプリン",
+    price:300
+},{
+    // 2
+    title:"愛玉ゼリー",
+    price:300
+},{
+    // 3
+    title:"黒胡麻あんゴマ団子",
+    price:480
+}];
+
+// 夏限定
+var Summer = [{
+    // 0
+    title:"冷やし中華",
+    price:780
+}];
+
+
+// ===== メニューカテゴリー =====
+
+var Menus = [{
+    title:"前菜",
+    list:Appetizer
+},{
+    title:"麺類",
+    list:Noodle
+},{
+    title:"ご飯類",
+    list:Rice
+},{
+    title:"海の幸料理",
+    list:Sea
+},{
+    title:"鶏肉料理",
+    list:Chicken
+},{
+    title:"豚肉料理",
+    list:Pig
+},{
+    title:"牛肉料理",
+    list:Cow
+},{
+    title:"豆腐料理",
+    list:Tofu
+},{
+    title:"野菜料理",
+    list:Veg
+},{
+    title:"点心",
+    list:Tensin
+},{
+    title:"デザート",
+    list:Desert
+},{
+    title:"夏限定",
+    list:Summer
+}]
+
+// ======================
+//    メニュー作成と表示
+// ======================
+
+$(function() {  
+    $.each(Menus, function(index, value){
+        var Menu = '<ul class="MenuList">' + '<p class="MenuTitle">' +value.title+ '</p>';
+        
+        $.each(value.list, function(index, value){
+            Menu += '<li>' + '<div class="MenuItem">';
+            Menu += '<p class="ItemName">' + value.title + '</p>'; 
+            Menu += '<p class="ItemPrice">' + "￥" + value.price + '</p>';
+            Menu += '</div>' + '</li>';
+        })
+        Menu += '</ul>';
+
+        $("#Menus").append(Menu);
+      })
+});
+  
